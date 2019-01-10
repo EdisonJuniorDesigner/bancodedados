c43f9c67a75dc10a98f7e0de83a7030439ae589c
EdisonJuniorDesigner <44264221+EdisonJuniorDesigner@users.noreply.github.com>
2019-01-10 11:45:15 -0200

    aa
    aa

--- Banco.sql
+++ Banco.sql
@@ -26,3 +26,39 @@ create table conta(
         cpf varchar (20) not null,
         foreign key (codagencia) references agencia (codagencia),
         foreign key (cpf) references cliente (cpf));
+					      
+insert into cidade(nome)values('Belford Roxo'),('Nova Iguaçu');
+insert into cliente(cpf, nome, enderco, telefone)values('887.856.547-96','Michael','Rua A','(21)3554-8564'),('997.456.877-26','Alana','Rua B','(21)3784-8521'),('255.451.354-87','Eduardo','Rua C','(21)2454-9464');
+insert into agencia(codagencia, nome, endereco, codcidade)values('357','Agencia Nova Iguaçu','Rua Nova',2),('358','Agencia Belford Roxo','Rua Belford',1);
+insert into conta(num_conta, saldo, codagencia, cpf)values(12,1500,'357','887.856.547-96'),(13,3000,'358','997.456.877-26'),(14,5000,'357','255.451.354-87');
+
+******* Busca todos os dados de todos os campos em uma tabela***
+select * from cidade;
+
+select * from cliente;
+
+select * from agencia;
+
+select * from conta;
+
+**** Pesquisar campos específicos******
+
+select cpf,nome,telefone from cliente;
+
+
+****** Apagar banco e tabelas****
+
+drop database nome banco --> apaga um banco
+
+drop table nometabela --> apaga uma tabela***
+
+
+***** altera a estrutura de um campo na tabela****
+
+alter table cliente modify nome varchar(65)not null;
+
+alter table agencia modify codagencia smallint not null auto_increment;
+
+
+obs.: quando alteramos um campo chave primária, é necessário desvincular as chaves
+estrangeiras.
